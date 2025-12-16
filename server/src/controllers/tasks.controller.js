f91348889652ec96dfcd54131888308d6e06dd4d
Aviv Bichachi <aviv.b@hotmail.com>
2025-12-16 20:40:27 +0200

    feat(api): add task routes and controller skeleton

---
+++ server/src/controllers/tasks.controller.js
@@ -0,0 +1,11 @@
+export function createTask(req, res) {
+  res.status(501).json({
+    message: "Create task - not implemented yet",
+  });
+}
+
+export function listTasks(req, res) {
+  res.status(501).json({
+    message: "List tasks - not implemented yet",
+  });
+}
